8f0802cf188cfbaffe422e0fc1c76544f517d8fe
betuldince <bedincer17@gmail.com>
2021-09-21 17:23:34 +0300

    First commit

---
+++ Assets/Scenes/scripts/playerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerController : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetTouch(0).phase==TouchPhase.Began) {
+            anim.SetBool("pass", true);
+        }
+        else
+        {
+            anim.SetBool("pass", false);
+        }
+    }
+}
